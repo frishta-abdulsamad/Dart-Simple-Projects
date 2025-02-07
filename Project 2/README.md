692de60a6509bcc7d761973fc587a76751e89071
Frishta <frishta.abdulsamad.2000@gmail.com>
2025-02-07 03:37:59 +0300

    Second Project: Simple Calculator

---
+++ Project 2/README.md
@@ -0,0 +1,15 @@
+Simple Calculator in Dart
+This is a command-line calculator built using Dart. It performs basic arithmetic operations such as addition (+), subtraction (-), multiplication (*), and division (/).
+
+Features:
+✅ Accepts two numbers as input.
+✅ Allows the user to choose an operator (+, -, *, /).
+✅ Performs the chosen operation and displays the result.
+✅ Prevents division by zero.
+✅ Handles invalid inputs and prompts the user until correct input is provided.
+
+Example usage:
+Enter the first operand: 10  
+Enter the second operand: 5  
+Enter an operator '(+, -, *, /)': *  
+10 * 5 = 50  
---
+++ Project 2/calculator..dart
@@ -0,0 +1,66 @@
+import 'dart:io';
+
+void main() {
+  num x, y;
+  while (true) {
+    stdout.write("Enter the first operand: ");
+    var operand1 = stdin.readLineSync();
+
+    if (operand1 == null || operand1.trim().isEmpty) {
+      print("The first operand can't be empty. Please enter a number");
+      continue;
+    }
+
+    try {
+      x = num.parse(operand1);
+      break;
+    } catch (e) {
+      print("Invalid input. Please enter a number");
+    }
+  }
+
+  while (true) {
+    stdout.write("Enter the second operand: ");
+    var operand2 = stdin.readLineSync();
+
+    if (operand2 == null || operand2.trim().isEmpty) {
+      print("The second operand can't be empty. Please enter a number");
+      continue;
+    }
+
+    try {
+      y = num.parse(operand2);
+      break;
+    } catch (e) {
+      print("Invalid operand. Please enter a number");
+    }
+  }
+
+  stdout.write("Enter an operator '(+, -, *, or /)': ");
+  var operator = stdin.readLineSync()!;
+  num result;
+  switch (operator) {
+      case ('+'):
+        result = x + y;
+        print("$x + $y = $result");
+        break;
+      case ('-'):
+        result = x - y;
+        print("$x - $y = $result");
+        break;
+      case ('*'):
+        result = x * y;
+        print("$x * $y = $result");
+        break;
+      case ('/'):
+        if (y != 0) {
+          result = x / y;
+          print("$x / $y = $result");
+        } else {
+          print("Error: Devision by zero.");
+        }
+        break;
+      default:
+        print("Invalid Operator");
+    }
+}
